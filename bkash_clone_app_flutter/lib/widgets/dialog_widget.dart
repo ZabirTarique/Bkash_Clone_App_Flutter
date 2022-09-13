3b0a524878ff0d2ee8e26bb2b34fab4965a11036
KMZabirTarique <k.m.zabirtarique.baust@gmail.com>
2022-09-14 00:40:44 +0600

    Solve the render flex issue

--- bkash_clone_app_flutter/lib/widgets/dialog_widget.dart
+++ bkash_clone_app_flutter/lib/widgets/dialog_widget.dart
@@ -29,45 +29,47 @@ class DialogWidget extends StatelessWidget {
       body: Column(children: [
         Expanded(child: Container(
           margin: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-          child: Column(children: [
-            Row(children: const [Padding(padding: EdgeInsets.fromLTRB(12,8,4,24), child: Text("Confirm to ", style: AppTheme.dialogconfirmText,),), Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0,8,12,24), child: Text("Send Money", style: AppTheme.dialogconfirmTextBold,),))],),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Row(children: const [Padding(padding: EdgeInsets.fromLTRB(12,8,4,24), child: Text("Confirm to ", style: AppTheme.dialogconfirmText,),), Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0,8,12,24), child: Text("Send Money", style: AppTheme.dialogconfirmTextBold,),))],),
 
-            Padding(padding: const EdgeInsets.fromLTRB(18, 12, 12, 24), child: Row(children: [
-              Image.asset("assets/user.png", height: 40, width: 40, fit: BoxFit.fitWidth,),
-              const SizedBox(width: 12,),
-              Expanded(child: Column(children: [
-                Row(children: const [Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0, 2, 0, 2), child: Text("Ryan Reynolds", style: AppTheme.ntitleText, textAlign: TextAlign.start,),)),
-                ],),
-                Row(children: const [Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0, 2, 0, 2), child: Text("01123456789", style: AppTheme.nbodyText, textAlign: TextAlign.start,),))],),
-              ],)),
-            ],),),
+              Padding(padding: const EdgeInsets.fromLTRB(18, 12, 12, 24), child: Row(children: [
+                Image.asset("assets/user.png", height: 40, width: 40, fit: BoxFit.fitWidth,),
+                const SizedBox(width: 12,),
+                Expanded(child: Column(children: [
+                  Row(children: const [Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0, 2, 0, 2), child: Text("Ryan Reynolds", style: AppTheme.ntitleText, textAlign: TextAlign.start,),)),
+                  ],),
+                  Row(children: const [Expanded(child: Padding(padding: EdgeInsets.fromLTRB(0, 2, 0, 2), child: Text("01123456789", style: AppTheme.nbodyText, textAlign: TextAlign.start,),))],),
+                ],)),
+              ],),),
 
-            Container(color: Colors.black12, width: double.infinity, height: 1,),
-            Padding(padding: const EdgeInsets.fromLTRB(0, 12, 0, 12), child: Row(children: [
-              Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
-                Text('Total'), SizedBox(height: 4,), Text('৳ 505.00')
-              ],),)),
-              Container(color: Colors.black12, width: 1, height: 50,),
-              Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
-                Text('New Balance'), SizedBox(height: 4,),  Text('৳ 6995.25')
-              ],),)),
-            ],),),
+              Container(color: Colors.black12, width: double.infinity, height: 1,),
+              Padding(padding: const EdgeInsets.fromLTRB(0, 12, 0, 12), child: Row(children: [
+                Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
+                  Text('Total'), SizedBox(height: 4,), Text('৳ 505.00')
+                ],),)),
+                Container(color: Colors.black12, width: 1, height: 50,),
+                Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
+                  Text('New Balance'), SizedBox(height: 4,),  Text('৳ 6995.25')
+                ],),)),
+              ],),),
 
-            Container(color: Colors.black12, width: double.infinity, height: 1,),
-            Padding(padding: const EdgeInsets.fromLTRB(0, 12, 0, 12), child: Row(children: [
-              Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
-                Text('Reference'), SizedBox(height: 4,), Text('N/A')
-              ],),)),
-              Container(color: Colors.black12, width: 1, height: 50,),
-              Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
-                Text(''), SizedBox(height: 4,),  Text('')
-              ],),)),
-            ],),),
+              Container(color: Colors.black12, width: double.infinity, height: 1,),
+              Padding(padding: const EdgeInsets.fromLTRB(0, 12, 0, 12), child: Row(children: [
+                Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
+                  Text('Reference'), SizedBox(height: 4,), Text('N/A')
+                ],),)),
+                Container(color: Colors.black12, width: 1, height: 50,),
+                Expanded(child: Padding(padding: const EdgeInsets.all(8), child: Column(children: const [
+                  Text(''), SizedBox(height: 4,),  Text('')
+                ],),)),
+              ],),),
 
-            Container(color: Colors.black12, width: double.infinity, height: 1,),
+              Container(color: Colors.black12, width: double.infinity, height: 1,),
 
-            Padding(padding: const EdgeInsets.all(12.0), child: Row(children: [Image.asset("assets/priyo.jpg", height: 40, width: 40,) , const Expanded(child: Padding(padding: EdgeInsets.fromLTRB(12,8,12,4), child: Text("You can send money free up to 25,000 Tk. monthly by adding Priyo number.", style: AppTheme.pinHintText,),))],),)
-          ],),
+              Padding(padding: const EdgeInsets.all(12.0), child: Row(children: [Image.asset("assets/priyo.jpg", height: 40, width: 40,) , const Expanded(child: Padding(padding: EdgeInsets.fromLTRB(12,8,12,4), child: Text("You can send money free up to 25,000 Tk. monthly by adding Priyo number.", style: AppTheme.pinHintText,),))],),)
+            ],),
+          ),
         )),
         AnimatedButton(
           // onComplete: _onConfirmed(context),
